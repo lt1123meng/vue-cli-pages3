43962defe15b388de221a15b4d4b725bced4dda7
LT <2581321506@qq.com>
2018-10-23 16:44:20 +0800

    10/23

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -57,7 +57,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
-        ? {safe: true, map: {inline: false}}
+        ? {safe: true, map: {inline: false}, autoprefixer: {remove: false}}
         : {safe: true}
     }),
     // generate dist index.html with correct asset hash for caching.
@@ -153,10 +153,8 @@ for (var pathname in modules) {
     template: modules[pathname],   // 模板路径
     inject: true              // js插入位置
   };
-  console.log(pathname)
   conf.chunks = ['vendor', 'manifest', pathname];
   conf.hash = true;
-  console.log(conf)
   webpackConfig.plugins.push(new HtmlWebpackPlugin(conf));
 }
 module.exports = webpackConfig
--- src/modules/welife-index/pages/shop/index.vue
+++ src/modules/welife-index/pages/shop/index.vue
@@ -1,9 +1,9 @@
 <template>
+  <div></div>
 </template>
 
 <script>
   import './index.styl'
-  import {IndexBanner, IndexNotice, IndexHot, IndexDurianList, IndexUnreadNumber} from '@/api/index.js'
   import UniFooter from '@/components/uni-footer/uni-footer'
   import DurianItem from '@/components/durian-item/durian-item'
 
